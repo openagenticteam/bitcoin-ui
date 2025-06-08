9a07b4a20be69926f52ee5046d161b486f86e757
OpenAgentic Team <thejackhudson+1@gmail.com>
2025-06-08 15:24:19 -0500

    Fix npm and qrcode.react build errors

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  clean: true,
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  sourcemap: true,
   dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  minify: true,
+  external: ["react", "react-dom", "qrcode.react"],
+  banner: {
+    js: '"use client";',
+  },
+  esbuildOptions(options) {
+    options.jsx = 'automatic'
+  },
 })
