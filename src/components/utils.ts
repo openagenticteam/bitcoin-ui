49b92ad49dd4dcc834d38b27003ff3c2bfc245ad
OpenAgentic Team <thejackhudson+1@gmail.com>
2025-06-09 07:55:21 -0500

    cleanup dependencies

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -145,7 +145,6 @@ export function clsx(...inputs: ClassValue[]): string {
   return classes.join(" ")
 }
 
-// Simplified version without tailwind-merge since we don't use Tailwind
 export function cn(...inputs: ClassValue[]): string {
   return clsx(...inputs)
 }
